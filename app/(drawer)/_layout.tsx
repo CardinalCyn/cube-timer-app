e7ee89a18fb54830a9cfd888ea9537726c43dce4
Salah Zanabili <Zanabilis@gmail.com>
2025-07-02 10:34:13 -0500

    set up basic pages, drawer functionality

---
+++ app/(drawer)/_layout.tsx
@@ -0,0 +1,25 @@
+import { Drawer } from "expo-router/drawer";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+
+export default function DrawerLayout() {
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Drawer>
+        <Drawer.Screen
+          name="index"
+          options={{
+            drawerLabel: "Home",
+            title: "Home",
+          }}
+        />
+        <Drawer.Screen
+          name="practice"
+          options={{
+            drawerLabel: "Practice",
+            title: "Practice",
+          }}
+        />
+      </Drawer>
+    </GestureHandlerRootView>
+  );
+}
